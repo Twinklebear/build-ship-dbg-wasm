d7d9926efd3573752489edc9f8fd6771c009def3
Will Usher <will@willusher.io>
2024-06-08 16:22:30 -0700

    minify code for post

--- cmake/FetchSDL2WebGPU.cmake
+++
@@ -1,8 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(
-  sdl2webgpu
-  GIT_REPOSITORY https://github.com/Twinklebear/sdl2webgpu.git
-  GIT_TAG main)
-
-FetchContent_MakeAvailable(sdl2webgpu)
--- web/src/index.ts
+++ web/src/index.ts
@@ -1,51 +1,17 @@
-import WGPUApp from "./cpp/wgpu_app.js";
-
-function sharedArrayBufferSupport() {
-  try {
-    var s = new SharedArrayBuffer(1024);
-    if (s === undefined) {
-      return false;
-    }
-  } catch (e) {
-    return false;
-  }
-  return true;
-}
+import CppApp from "./cpp/app.js";
 
 (async () => {
   let canvas = document.getElementById("canvas");
-  if (navigator.gpu === undefined) {
-    canvas.setAttribute("style", "display:none;");
-    document
-      .getElementById("no-webgpu")
-      .setAttribute("style", "display:block;");
-    return;
-  }
-
-  /*
-  if (!sharedArrayBufferSupport()) {
-    canvas.setAttribute("style", "display:none;");
-    document
-      .getElementById("no-shared-array-buffer")
-      .setAttribute("style", "display:block;");
-    return;
-  }
-  */
 
   // Block right click so we can use right click + drag to pan
   canvas.addEventListener("contextmenu", (evt: Event) => {
     evt.preventDefault();
   });
 
-  // Get a GPU device to render with
-  let adapter = await navigator.gpu.requestAdapter();
-  let device = await adapter.requestDevice();
-
   // We set -sINVOKE_RUN=0 when building and call main ourselves because something
   // within the promise -> call directly chain was gobbling exceptions
   // making it hard to debug
-  let app = await WGPUApp({
-    preinitializedWebGPUDevice: device,
+  let app = await CppApp({
     canvas,
   });
 
