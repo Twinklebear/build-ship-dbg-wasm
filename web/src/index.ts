bfb0f016140e200f1f827c98b9bfeee0a31d208f
Will Usher <will@willusher.io>
2024-06-08 17:03:48 -0700

    not needed for this demo

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,13 +1,6 @@
 import CppApp from "./cpp/app.js";
 
 (async () => {
-  let canvas = document.getElementById("canvas");
-
-  // Block right click so we can use right click + drag to pan
-  canvas.addEventListener("contextmenu", (evt: Event) => {
-    evt.preventDefault();
-  });
-
   // We set -sINVOKE_RUN=0 when building and call main ourselves because something
   // within the promise -> call directly chain was gobbling exceptions
   // making it hard to debug
